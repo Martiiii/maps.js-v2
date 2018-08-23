82d8142c7af7d3c6c3ad302f732002203937c7b7
martimutso <marti.mutso@gmail.com>
2018-08-23 20:51:37 +0300

    Babel setup. Initial code, can create multiple map instances.

---
+++ src/maps.js
@@ -0,0 +1,61 @@
+const maps = {};
+maps.mapsList = [];
+
+maps.initMaps = () => {
+	maps.mapsList.forEach((map) => {
+		map.initialize();
+	});
+};
+
+window.initMaps = maps.initMaps;
+
+maps.GMap = class GMap {
+	constructor(apiKey, mapDivId) {
+		if (maps.mapsList.length === 0) {
+			const script = document.createElement('script');
+			script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=geometry&callback=initMaps`;
+			script.defer = true;
+			script.async = true;
+			document.getElementsByTagName('head')[0].appendChild(script);
+		}
+		this._map;
+		this.mapDivId = mapDivId;
+
+		maps.mapsList.push(this);
+	}
+
+	createMap = (idOfDiv, mapType) => {
+		return new google.maps.Map(document.getElementById(idOfDiv), {
+			mapTypeId: mapType,
+			mapTypeControl: true,
+			center: new google.maps.LatLng(59.436962, 24.753574),
+			zoom: 7,
+			scrollwheel: false
+		});
+	};
+
+	_processPoints = (geometry, callback, thisArg) => {
+		if (geometry instanceof google.maps.LatLng) {
+			callback.call(thisArg, geometry);
+		} else if (geometry instanceof google.maps.Data.Point) {
+			callback.call(thisArg, geometry.get());
+		} else {
+			geometry.getArray().forEach((g) => {
+				this._processPoints(g, callback, thisArg);
+			});
+		}
+	};
+
+	fitAreasOnScreen = () => {
+		const bounds = new google.maps.LatLngBounds();
+		this._map.data.addListener('addfeature', (e) => {
+			this._processPoints(e.feature.getGeometry(), bounds.extend, bounds);
+			this._map.fitBounds(bounds);
+		});
+	};
+
+	initialize = () => {
+		this._map = this.createMap(this.mapDivId, 'hybrid');
+		this.fitAreasOnScreen();
+	};
+};
